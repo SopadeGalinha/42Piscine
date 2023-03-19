5f10761a9f9c605b769ca4af50dba826c3c0be39
SopadeGalinha <jhsocial@icloud.com>
2023-03-19 19:56:52 +0000

    Rush01

--- Piscine/Rush/rush00.c
+++ Piscine/Rush/rush00.c
@@ -9,3 +9,14 @@
 /*   Updated: 2023/03/15 20:53:45 by jhogonca         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+void    ft_print(int x, int y)
+{
+    int i = 1;
+    putchar('o');
+    while (++i < x)
+        putchar(' ');
+    if 
+}
--- Piscine/Rush/rush01.c
+++ Piscine/Rush/rush01.c
@@ -1,41 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   testes.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jhogonca <jhogonca@student.42lyon.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/18 03:57:58 by jhogonca          #+#    #+#             */
-/*   Updated: 2023/03/18 03:57:58 by jhogonca         ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void	ft_putchar(char c)
 {
 	write (1, &c, 1);
-}
-
-void	ft_print_table(int row[][4])
-{
-	int	i;
-	int	c;
-
-	i = -1;
-	i = -1;
-	while (++i < 4)
-	{
-		c = -1;
-		while (++c < 4)
-		{
-			ft_putchar(row[i][c] + '0');
-			ft_putchar(' ');
-		}
-		ft_putchar('\n');
-	}
 }
 
 char	*ft_strncpy(char *dest, char *src, int n)
@@ -56,153 +27,22 @@ char	*ft_strncpy(char *dest, char *src, int n)
 	return (dest);
 }
 
-int	ft_atoi(char *str)
-{
-	int	sign;
-	int	result;
-
-	sign = 1;
-	result = 0;
-
-	while ((*str >= 9 && *str <= 13) || *str == 32)
-		str++;
-	if (*str == '-')
-		sign = sign * -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
-		result = result * 10 + *str++ - '0';
-	return (result * sign);
-}
-
-// retorna falso se encontrar o valor 
-int	ft_verify_cell(int tab[][4], int line, int column, int value)
-{
-	int temp;
-
-	temp = -1;
-	while(++temp < 4) // verificar linha
-		if(tab[temp][column] == value)
-			return (0);
-	temp = -1;
-	while (++temp < 4) // verifica coluna
-		if (tab[line][temp] == value)
-			return (0);
-	return (1);
-}
-
-void	ft_check_four_vertical(int board[][4], char **tab)
+void	ft_print_table(int board[4][4])
 {
 	int	i;
 	int	j;
-	int	k;
 
 	i = -1;
 	while (++i < 4)
 	{
-		if (tab[0][i] == '4')
+		j = -1;
+		while (++j < 4)
 		{
-			j = -1;
-			while (++j < 4)
-				board[j][i] = j + 1;
+			ft_putchar(board[i][j] + '0');
+			ft_putchar(' ');
 		}
-		if (tab[1][i] == '4')
-		{
-			j = 4;
-			k = 0;
-			while (--j >= 0)
-				board[j][i] = ++k;
-		}
+		ft_putchar('\n');
 	}
-
-}
-
-void	ft_check_four_horizontal(int board[][4], char **tab)
-{
-	int	i;
-	int	j;
-	int	k;
-
-	i = -1;
-	while (++i < 4)
-	{
-		if (tab[2][i] == '4')
-		{
-			j = -1;
-			while (++j < 4)
-				board[i][j] = j + 1;
-		}
-		if (tab[3][i] == '4')
-		{
-			j = 4;
-			k = 0;
-			while (--j >= 0)
-				board[i][j] = ++k;
-		}
-	}
-
-}
-
-void	ft_fillfour(int board[][4], char **tab)
-{
-	int	column;
-
-	column = -1;
-	while (tab[0][++column])
-	{
-		if (tab[0][column] == '1')		//VERIFY COLUMNS
-			board[0][column] = 4;
-		if (tab[1][column] == '1')		//VERIFY COLUMNS
-			board[3][column] = 4;
-		if (tab[2][column] == '1')		//VERIFY ROWS
-			board[column][0] = 4;
-		if (tab[3][column] == '1')		//VERIFY ROWS
-			board[column][3] = 4;
-	}
-	if (ft_verify_cell(board, 1, 1, 4) && (tab[1][1] == '3' || tab[3][1] == '3'))
-		board[1][1] = 4;
-	if (ft_verify_cell(board, 1, 2, 4) && (tab[1][2] == '3' || tab[2][1] == '3'))
-		board[1][2] = 4;
-	if (ft_verify_cell(board, 2, 1, 4) && (tab[0][1] == '3' || tab[3][2] == '3'))
-		board[2][1] = 4;
-	if (ft_verify_cell(board, 2, 2, 4) && (tab[0][2] == '3' || tab[2][2] == '3'))
-		board[2][2] = 4;
-}
-
-void	random_solutions(int board[][4], char **tab)
-{
-	int i;
-	int j;
-
-	i = -1;
-	j = -1;
-	while (++i < 4)
-	{
-		if (tab[0][i] == '2' && tab[1][i] == '1')
-			board[0][i] = 3;
-		if (tab[1][i] == '2' && tab[0][i] == '1')
-			board[3][i] = 3;
-		if (tab[0][i] == '3' && !ft_verify_cell(board, i, 0, 1))
-			board[0][i] == 2;
-		if (tab[0][i] == '3' && !ft_verify_cell(board, i, 0, 2))
-			board[0][i] == 1;
-		if (tab[1][i] == '3' && !ft_verify_cell(board, i, 3, 1))
-			board[3][i] == 2;
-		if (tab[1][i] == '3' && !ft_verify_cell(board, i, 3, 2))
-			board[3][i] == 1;
-	}
-	
-}
-
-void	ft_interation(int board[][4], char **tab)
-
-
-void	ft_do_the_things(int board[][4], char **tab)
-{
-	ft_fillfour(board, tab);
-	ft_check_four_vertical(board, tab);
-	ft_check_four_horizontal(board, tab);
-	random_solutions(board, tab);
 }
 
 char	**ft_filltab(char **columns, char *src)
@@ -250,13 +90,175 @@ char	**ft_malloc(char *src)
 	return (ft_filltab(columns, src));
 }
 
+int	ft_verify_column_t0(int board[4][4], char **tab)
+{
+	int	column;
+	int	j;
+	int	count;
+	int	i;
+
+	column = -1;
+	while (++column < 4)
+	{
+		i = -1;
+		count = 1;
+		while (++i < 4)
+		{
+			if (board[i][column] == 4)
+				break ;
+			if (board[i][column] < board[i + 1][column])
+				count++;
+		}
+		if (tab[0][column] != count + '0')
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_verify_column_t1(int board[4][4], char **tab)
+{
+	int	column;
+	int	j;
+	int	count;
+	int	i;
+
+	column = 4;
+	while (--column >= 0)
+	{
+		i = 4;
+		count = 1;
+		while (--i >= 0)
+		{
+			if (board[i][column] == 4)
+				break ;
+			if (board[i][column] < board[i - 1][column])
+				count++;
+		}
+		if (tab[1][column] != count + '0')
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_verify_row_t3(int board[4][4], char **tab)
+{
+	int	row;
+	int	j;
+	int	count;
+	int	i;
+
+	row = 4;
+	while (--row >= 0)
+	{
+		i = 4;
+		count = 1;
+		while (--i >= 0)
+		{
+			if (board[row][i] == 4)
+				break ;
+			if (board[row][i] < board[row][i - 1])
+				count++;
+		}
+		if (tab[3][row] != count + '0')
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_verify_row_t2(int board[4][4], char **tab)
+{
+	int	row;
+	int	j;
+	int	count;
+	int	i;
+
+	row = -1;
+	while (++row < 4)
+	{
+		i = -1;
+		count = 1;
+		while (++i < 4)
+		{
+			if (board[row][i] == 4)
+				break ;
+			if (board[row][i] < board[row][i + 1])
+				count++;
+		}
+		if (tab[2][row] != count + '0')
+			return (0);
+	}
+	return (1);
+}
+
+
+
+
+int is_valid(int matrix[4][4], int row, int col, int num) 
+{
+	int i;
+		
+	i = -1;
+    // Verifica linha
+    while (++i < 4) 
+	{
+        if (matrix[row][i] == num) 
+		{
+            return (0);
+        }
+    }
+	i = -1;
+    // Verifica coluna
+    while (++i < 4) 
+	{
+        if (matrix[i][col] == num) 
+		{
+            return (0);
+        }
+    }
+    return (1);
+}
+
+void ft_combinations(int matrix[4][4], int row, int col, char **tab) 
+{
+	int num;
+	int static c;
+
+	c = 0;
+    if (row == 4) 
+	{
+	    if (ft_verify_row_t3(matrix, tab) == 1)
+			if (ft_verify_column_t0(matrix, tab) == 1) 
+				if (ft_verify_column_t1(matrix, tab) == 1)
+					ft_print_table(matrix);
+	}			
+					
+    if (col == 4)
+	{
+        // Proxima linha
+		ft_combinations(matrix, row + 1, 0, tab);
+        return;
+    }
+	
+	num = 0;
+    // Tenta os numeros de 1 a 4 na celula
+    while (++num <= 4) 
+	{
+        if (is_valid(matrix, row, col, num)) 
+		{
+            matrix[row][col] = num;
+            ft_combinations(matrix, row, col + 1, tab);
+            matrix[row][col] = 0;
+        }
+    }
+}
 
 int main(int ac, char **av)
 {
-	int		row[4][4];
+	int		board[4][4];
 	int		c;
 	int		i;
 	char	dest[16];
+	char	**tab;
  
 	if (ac < 2)
 		return (0);
@@ -265,14 +267,15 @@ int main(int ac, char **av)
 	{
 	  c = -1;
 	  while (++c < 4)
-	    row[i][c] = 0;
+	    board[i][c] = 0;
 	}
-	i = -1;
 	ft_strncpy(dest, av[1], 16);
-	av = ft_malloc(dest);
-	ft_do_the_things(row, av);
-	ft_print_table(row);
-	while (av[++i])
-		free(av[i]);
-	free(av);
+	tab = ft_malloc(dest);
+	ft_combinations(board, 0, 0, tab);
+	i = 4;
+	while (--i >= 0)
+		while (tab[i])
+	 		free(tab[i]);
+	free(tab);
+	return (0);
 }
